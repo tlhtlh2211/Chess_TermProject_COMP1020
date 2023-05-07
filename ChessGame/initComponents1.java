de63f72ffc13427c0426df39ce7b584038e8e0ac
Haiiiiii <89590444+Haiiiiii@users.noreply.github.com>
2023-05-07 22:38:09 +0700

    Change

--- ChessGame/initComponents1.java
+++ ChessGame/initComponents1.java
@@ -5,8 +5,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
 public class initComponents1 extends JFrame implements ActionListener{
 
