6e905876d2dd20575a4182e52eca97845175180b
datdang2712 <100905322+datdang2712@users.noreply.github.com>
2023-05-18 23:33:23 +0700

    Update Stalemate.java

--- ChessGame/Stalemate.java
+++ ChessGame/Stalemate.java
@@ -1,4 +1,6 @@
 package ChessGame;
+import java.util.ArrayList;
+
 import Piece.*;
 
 public class Stalemate {
@@ -12,8 +14,28 @@ public class Stalemate {
     {
         int sameMoves = 0;
         Movement[] moves = new Movement[12];
+
         ChessScanner scanner = new ChessScanner(board);
+
+        int countWhite;
+        int countBlack;
+
         if (board.pieceList.size() <= 2) return true;
+       
+        if (board.countBlackPiece <= 2 && board.countWhitePiece <= 2)
+        {
+            countBlack = 0;
+            countWhite = 0;
+            for (Piece piece : board.pieceList)
+            {
+                if (piece.name.equals("Knight") || piece.name.equals("Bishop"))
+                {
+                    if (piece.isWhite) countWhite++;
+                    else countBlack++;
+                }
+            }
+            if (countBlack == 1 && countWhite == 1) return true;
+        }
         for (int i = 0; i < 8; i ++)
         {
             for (int j = 0; j < 8; j++)
@@ -24,6 +46,11 @@ public class Stalemate {
                 }
             }
         }
+
+
+        if (board.undoList.empty()) return false;
+        if (board.undoList.size() < 12) return false; 
+
         for (int j = 0; j < 12; j++)
         {
             Movement move = board.undoList.pop();
@@ -31,6 +58,7 @@ public class Stalemate {
         }
         for (int z = 0; z < 4; z++)
         {
+            sameMoves = 0;
             if (moves[z].piece.name.equals(moves[z + 4].piece.name) && moves[z].piece.name.equals(moves[z + 8].piece.name))
             {
                 if ((moves[z].newColumn == moves[z + 4].newColumn && moves[z].newRow == moves[z + 4].newRow)
@@ -41,6 +69,7 @@ public class Stalemate {
                 return true;
             }
         }
+        
         return false;
     }
 
